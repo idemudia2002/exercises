0df4e244ed90305da8e6882e82ac2ff122c8c2fb
idemudia2002 <idde19@yahoo.com>
2019-12-12 23:21:52 +0100

    First commit

---
+++ uppgift_2.py
@@ -0,0 +1,7 @@
+def questionify(x):
+    return x + "? "
+  
+print(questionify)
+ 
+
+print(questionify("jag heter einar"))
---
+++ uppgift_3.py
@@ -0,0 +1,8 @@
+
+
+def devisible_by_five(x):
+    if x % 5 == 0:
+        return True
+    else:
+        return False
+print(devisible_by_five(10))
